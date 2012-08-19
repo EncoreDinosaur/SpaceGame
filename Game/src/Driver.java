be2469ee14abfae7caadaf96e724a0d0c2dc3670
Longwashere <Long.p.hoang@aim.com>
2012-08-19 03:14:30 -0700

    Update Game/src/Driver.java

--- Game/src/Driver.java
+++ Game/src/Driver.java
@@ -1,9 +1,8 @@
 import javax.swing.JFrame;
 public class Driver {
 	public static void main(String[] args){
-		Map game = new Map(450,600);
+		Game game = new Game(450,600);
 		JFrame map = new JFrame();
-		
 		map.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		map.setVisible(true);
 		map.setContentPane(game);
